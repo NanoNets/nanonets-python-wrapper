3e6d16f74a5ad2cf90fe990b2c1bcb51ba6c92ba
Anuj <anuj@Anujs-Macbook.local>
2019-10-03 18:49:09 +0530

    minor

--- build/lib/nanonets/__init__.py
+++
@@ -1,6 +0,0 @@
-import nanonets.utils
-from nanonets.model import Model
-from nanonets.classification import ImageClassification
-from nanonets.multilabel_classification import MultilabelClassification
-from nanonets.object_detection import ObjectDetection
-from nanonets.ocr import OCR 
