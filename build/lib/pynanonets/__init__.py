9475d72608f981fcccd333a9d642210fc38bf6b2
Anuj <anuj@Anujs-Macbook.local>
2019-10-04 13:55:28 +0530

    readme edits

--- build/lib/pynanonets/__init__.py
+++
@@ -1,6 +0,0 @@
-import pynanonets.utils
-from pynanonets.model import Model
-from pynanonets.classification import ImageClassification
-from pynanonets.multilabel_classification import MultilabelClassification
-from pynanonets.object_detection import ObjectDetection
-from pynanonets.ocr import OCR 
